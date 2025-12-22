3608fb1b4fa2793600399e62ce31c3557fcb0b61
Lorenous <osamaabaelziz@gmail.com>
2025-12-23 01:25:29 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:islami/screens/home_screen.dart';
+import 'package:islami/screens/introduction_screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: IntroductionScreens.routeName,
+      routes: {
+        IntroductionScreens.routeName: (context) => IntroductionScreens(),
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/introduction_screens.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IntroductionScreens extends StatelessWidget {
+  const IntroductionScreens({super.key});
+  static const String routeName = 'introduction-screens';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
