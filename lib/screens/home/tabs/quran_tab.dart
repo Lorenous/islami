654cff3b78ffe810d028fe936d3bafef73328203
Lorenous <osamaabaelziz@gmail.com>
2025-12-23 05:52:58 +0200

    set quran tab background image

--- lib/screens/home/tabs/quran_tab.dart
+++ lib/screens/home/tabs/quran_tab.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:islami/core/app_colors.dart';
 
 class QuranTab extends StatelessWidget {
   const QuranTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        Container(
+          height: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images/quranbg.jpg'),
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(color: AppColors.dark.withAlpha(158)),
+        ),
+      ],
+    );
   }
 }
