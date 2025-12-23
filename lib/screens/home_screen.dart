357f924cdf29b936f85821ab924d803a86024249
Lorenous <osamaabaelziz@gmail.com>
2025-12-23 04:25:32 +0200

    create bottom vanigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:islami/core/app_colors.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
   static const String routeName = 'home-screen';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: AppColors.primaryColor,
+        selectedItemColor: AppColors.white,
+        unselectedItemColor: AppColors.dark,
+        showUnselectedLabels: false,
+        currentIndex: index,
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: index == 0
+                ? Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: AppColors.dark.withAlpha(153),
+                      borderRadius: BorderRadius.circular(66),
+                    ),
+                    child: ImageIcon(AssetImage('assets/images/ic_quran.png')),
+                  )
+                : ImageIcon(AssetImage('assets/images/ic_quran.png')),
+            label: 'Quran',
+          ),
+          BottomNavigationBarItem(
+            icon: index == 1
+                ? Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: AppColors.dark.withAlpha(153),
+                      borderRadius: BorderRadius.circular(66),
+                    ),
+                    child: ImageIcon(AssetImage('assets/images/ic_hadeth.png')),
+                  )
+                : ImageIcon(AssetImage('assets/images/ic_hadeth.png')),
+            label: 'Hadith',
+          ),
+          BottomNavigationBarItem(
+            icon: index == 2
+                ? Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: AppColors.dark.withAlpha(153),
+                      borderRadius: BorderRadius.circular(66),
+                    ),
+                    child: ImageIcon(AssetImage('assets/images/ic_sebha.png')),
+                  )
+                : ImageIcon(AssetImage('assets/images/ic_sebha.png')),
+            label: 'Sebha',
+          ),
+          BottomNavigationBarItem(
+            icon: index == 3
+                ? Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: AppColors.dark.withAlpha(153),
+                      borderRadius: BorderRadius.circular(66),
+                    ),
+                    child: ImageIcon(AssetImage('assets/images/ic_radio.png')),
+                  )
+                : ImageIcon(AssetImage('assets/images/ic_radio.png')),
+            label: 'Radio',
+          ),
+          BottomNavigationBarItem(
+            icon: index == 4
+                ? Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: AppColors.dark.withAlpha(153),
+                      borderRadius: BorderRadius.circular(66),
+                    ),
+                    child: ImageIcon(AssetImage('assets/images/ic_time.png')),
+                  )
+                : ImageIcon(AssetImage('assets/images/ic_time.png')),
+            label: 'Time',
+          ),
+        ],
+      ),
+    );
   }
 }
