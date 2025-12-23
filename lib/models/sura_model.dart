93b0bb930a9f964aa5c0f07740adbc58812258f5
Lorenous <osamaabaelziz@gmail.com>
2025-12-23 17:28:37 +0200

    create sura model

---
+++ lib/models/sura_model.dart
@@ -0,0 +1,10 @@
+class SuraModel {
+  final String nameAr, nameEn;
+  final int versesNum;
+
+  SuraModel({
+    required this.nameAr,
+    required this.nameEn,
+    required this.versesNum,
+  });
+}
