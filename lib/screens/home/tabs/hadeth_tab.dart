a1ceebcd7e8ee3564c1d42b70dd45495320ce359
Lorenous <osamaabaelziz@gmail.com>
2025-12-25 09:50:18 +0200

    create hadeth card

--- lib/screens/home/tabs/hadeth_tab.dart
+++ lib/screens/home/tabs/hadeth_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:islami/core/app_colors.dart';
+import 'package:islami/core/app_styles.dart';
 
 class HadethTab extends StatelessWidget {
   const HadethTab({super.key});
@@ -15,7 +16,7 @@ class HadethTab extends StatelessWidget {
         ),
       ),
       child: Container(
-        padding: EdgeInsets.all(16),
+        padding: EdgeInsets.symmetric(vertical: 16),
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [AppColors.dark.withAlpha(158), AppColors.dark],
@@ -31,6 +32,77 @@ class HadethTab extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class HadethCard extends StatelessWidget {
+  const HadethCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: AppColors.primaryColor),
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 16, right: 8, left: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      'assets/images/left_corner.png',
+                      color: AppColors.dark,
+                    ),
+                    Image.asset(
+                      'assets/images/right_corner.png',
+                      color: AppColors.dark,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Opacity(
+                  opacity: 0.3,
+                  child: Image.asset('assets/images/hadeth_card_bg.png'),
+                ),
+              ),
+              Image.asset(
+                'assets/images/bottom_decoration.png',
+                color: AppColors.dark.withAlpha(200),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 32),
+            child: Column(
+              children: [
+                Text(
+                  'الحديث الأول',
+                  style: AppStyles.bold24.copyWith(color: AppColors.dark),
+                ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Text(
+                        " عن أمـيـر المؤمنـين أبي حـفص عمر بن الخطاب رضي الله عنه ، قال : سمعت رسول الله صلى الله عـليه وسلم يـقـول : ( إنـما الأعـمـال بالنيات وإنـمـا لكـل امـرئ ما نـوى . فمن كـانت هجرته إلى الله ورسولـه فهجرتـه إلى الله ورسـوله ومن كانت هجرته لـدنيا يصـيبها أو امرأة ينكحها فهجرته إلى ما هاجر إليه ).رواه إمام المحد ثين أبـو عـبـد الله محمد بن إسماعـيل بن ابراهـيـم بن المغـيره بن بـرد زبه البخاري الجعـفي،[رقم:1] وابـو الحسـيـن مسلم بن الحجاج بن مـسلم القـشـيري الـنيسـابـوري [رقم :1907] رضي الله عنهما في صحيحيهما اللذين هما أصح الكتب المصنفه.",
+                        style: TextStyle(
+                          color: AppColors.dark,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
